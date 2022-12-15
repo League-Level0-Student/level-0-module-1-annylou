d3e17edf875316e3668074fb047782f0c9530212
league <league@imac-24.attlocal.net>
2022-12-14 16:59:05 -0800

    remarkable

--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -8,7 +8,7 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-String remarkable = "is 123";
+String remarkable = " is 123";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
 String name = JOptionPane.showInputDialog("enter a name");
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
